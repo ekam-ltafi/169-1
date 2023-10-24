1715812a9cf90ecac526cec6b41dcf3c73f9823e
ekam-ltafi <101791160+ekam-ltafi@users.noreply.github.com>
2023-10-24 19:38:04 +0530

    Update 169-1.py

--- 169-1.py
+++ 169-1.py
@@ -1,22 +1,22 @@
 class Bookshelf :
-    def __ini__(self , name , author , price , publishing_year):
-        self.book_name = name
-        self.book_author = author 
-        self.book_price = price
-        self.book_publishing_year = publishing_year
-        
-    def add_book(self):
-        print("Book Name : " + self.book_name)
-        print("Author Name : " + self.book_author)
-        print("Book Price : " + self.book_price)
-        print(" Publishing Date : " + self.book_publishing_date)
-        print("Book has been added to the library ")
-    
-    def years_since_publish(self):
-        years_ago_published = 2023 - self.book_publishing_year
-        print("This book was published in " + str(years_ago_published))
-        
-        
+  def __init__(self , name , author , price , publishing_year):
+    self.book_name = name
+    self.book_author = author 
+    self.book_price = price
+    self.book_publishing_year = publishing_year
+     
+  def add_book(self):
+    print("Book Name : " + self.book_name)
+    print("Author Name : " + self.book_author)
+    print("Book Price : " + str(self.book_price))
+    print(" Publishing Date : " + str(self.book_publishing_year))
+    print("Book has been added to the library ")
+   
+  def years_since_publish(self):
+    years_ago_published = 2023 - self.book_publishing_year
+    print("This book was published in " + str(years_ago_published))
+     
+     
 Book1 = Bookshelf("Harry Potter and Philosopher's Stone ", "J.K Rowling ",2000 , 1997)
 Book1.add_book()
 Book1.years_since_publish()
